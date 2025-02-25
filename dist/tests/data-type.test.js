a6657e3452e8a14fd806521d3b6f02f60b0fcbfb
Faldiansyah <faldiansyahk@gmail.com>
2025-02-25 08:32:06 +0700

    primitive data types

---
+++ dist/tests/data-type.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('Data Type', function () {
+    it('should must declare', function () {
+        const name = "Ahmad Ali";
+        const balance = 150000;
+        const isVip = false;
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
---
+++ tests/data-type.test.ts
@@ -0,0 +1,11 @@
+describe('Data Type', function () {
+    it('should must declare', function () {
+        const name: string = "Ahmad Ali";
+        const balance: number = 150000;
+        const isVip: boolean = false;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+})
